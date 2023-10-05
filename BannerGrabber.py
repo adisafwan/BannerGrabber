daf90428865e2597df492daf2e87c868337fda4b
Adi Safwan <adisafwan20082001@gmail.com>
2023-10-05 11:00:43 +0800

    Update BannerGrabber.py

--- BannerGrabber.py
+++ BannerGrabber.py
@@ -1,1 +1,12 @@
+import socket
+
+# Declare s from socket module as socket object
+s = socket.socket()
+
+# Prompt user to enter IP address and port number
+ip = input("Please enter the IP address: ")
+port = input ("Please enter the port number: ")
+
+# Connecting the ip and port to socket
+s.connect((ip, port))
 
